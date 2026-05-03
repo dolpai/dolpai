7e229b8e0d13c788eb6a4443af54b4a6b1e24bee
Dolpai <mrproductionnp@gmail.com>
2026-05-04 02:55:58 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,1 @@
-import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		})
-	}
-};
-
-export default config;
